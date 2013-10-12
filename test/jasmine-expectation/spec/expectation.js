2556b31a447ac528309464a202de459935304972
tlin <teddy.lin@morningstar.com>
2013-10-12 17:03:30 +0800

    new things

--- test/jasmine-expectation/spec/expectation.js
+++ test/jasmine-expectation/spec/expectation.js
@@ -256,3 +256,30 @@ describe("A spy", function() {
 		expect(bar).toBeNull();
 	});
 });
+
+
+
+describe("Asynchronous specs", function() {
+	var value, flag;
+
+	it("should support async execution of test preparation and exepectations", function() {
+
+		runs(function() {
+			flag = false;
+			value = 0;
+
+			setTimeout(function() {
+				flag = true;
+			}, 3000);
+		});
+
+		waitsFor(function() {
+			value++;
+			return flag;
+		}, "The Value should be incremented", 4000);
+
+		runs(function() {
+			expect(value).toBeGreaterThan(0);
+		});
+	});
+});
