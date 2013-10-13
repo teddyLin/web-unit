6b262f91ed631cbc3519f0d585cea75fed549a23
teddy lin <web_teddy@163.com>
2013-10-14 00:38:16 +0800

    more things

---
+++ main/config/test-case-config.js
@@ -0,0 +1,10 @@
+(function() {
+
+	// 这部分可以扫描得到
+	var tc = [
+		"D:/javaCoding/www/web-unit/test/jasmine-expectation/xtest.html",
+		"D:/javaCoding/www/web-unit/test/calc-lib/xtest.html"
+	]
+	window.tc = tc;
+	
+})(window);
